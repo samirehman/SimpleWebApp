9d643c2c314aff7f8c24ab4fcfa48d0b06e7ff85
samirehman <samidba@gmail.com>
2016-07-12 01:02:20 +0100

    This is the first test change

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -8,6 +8,10 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("error")) {
+            return "This is an error but we did it on purpose.";
+        }
+
         return "";
     }
 }
